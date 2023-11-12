70c99d4a10c77e67bf69d681dba3b8ed8a2fdea6
PSKutergin <PSKutergin@gmail.com>
2023-11-12 07:18:22 +0300

    add header and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,46 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./assets/logo.png" alt="Logo" class="h-9" />
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="false" class="flex items-center gap-1">
+        Day complete
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
+
+  <main class="flex flex-grow flex-col">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, distinctio.
+  </main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a href="#timeline" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ClockIcon class="h-6 w-6" /> timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#activities" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ListBulletIcon class="h-6 w-6" /> activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#progress" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ChartBarIcon class="h-6 w-6" /> progress
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
