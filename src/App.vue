4fc1d227cd8afd5522300410784dc95909b4ec9b
PSKutergin <PSKutergin@gmail.com>
2023-12-13 10:41:09 +0300

    add module with general funcs

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,10 @@ import TheTimeline from './pages/TheTimeline.vue'
 import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
 import { ref } from 'vue'
+import { normalizePageHash } from './functions'
 import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from './constants'
 
 const currentPage = ref(normalizePageHash())
-
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-
-  if (Object.keys([PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE]).includes(hash)) {
-    return hash
-  }
-
-  window.location.hash = PAGE_TIMELINE
-
-  return PAGE_TIMELINE
-}
 
 function goTo(page) {
   currentPage.value = page
