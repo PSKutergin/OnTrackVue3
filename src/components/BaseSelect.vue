cf8ef6edeb7af699b6b68957f20e9b58d7901acb
PSKutergin <PSKutergin@gmail.com>
2023-12-15 23:00:57 +0300

    add congiguration for props

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BaseButton from './BaseButton.vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+
+// defineProps(['selected', 'options', 'placeholder'])
+
+defineProps({
+  selected: Number,
+  placeholder: {
+    required: true,
+    type: String
+  },
+  options: {
+    required: true,
+    type: Array
+  }
+})
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <BaseButton><XMarkIcon class="h-8" /></BaseButton>
+    <select class="rouded w-full truncate bg-gray-100 px-2 py-1 text-2xl">
+      <option selected disabled value="">{{ placeholder }}</option>
+      <option
+        v-for="{ value, label } in options"
+        :key="value"
+        :value="value"
+        :selected="value === selected"
+      >
+        {{ label }}
+      </option>
+    </select>
+  </div>
+</template>
